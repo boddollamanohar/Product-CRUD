a2e0fdfdbd644ff2e048ce6df4b1d4409fa58a4c
unknown <iambmanohar@gmail.com>
2023-02-28 15:18:17 +0530

    data

--- my-client/src/App.js
+++ my-client/src/App.js
@@ -1,10 +1,21 @@
 import React from 'react'
-import Navbar from './navbar/navbar'
-
+import Product from './Products/Product'
+import Admin from './Products/Admin'
+import '../node_modules/bootstrap/dist/css/bootstrap.css'
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
+import Navbar from './navbar/Navbar'
+import About from './Products/About'
 const App = () => {
   return (
     <div>
-      <Navbar/>
+      <Router>
+        <Navbar />
+        <Switch>
+          <Route path="/product" component={Product} />
+          <Route path="/admin" component={Admin} />
+          <Route path="/About" component={About} />
+        </Switch>
+      </Router>
     </div>
   )
 }
--- my-client/src/navbar/navbar.jsx
+++ my-client/src/navbar/navbar.jsx
@@ -1,24 +1,21 @@
-import React, { Component } from 'react'
-class Navbar extends Component {
-  render() {
-    return (
-        <nav class="navbar navbar-dark bg-dark navbar-expand-lg">  
-        {/* <a href="#" class="navbar-brand"> Manohar_Tech</a> */}
-        <div class="ml-auto">
-            <ul class="navbar-nav">
-                <li class="nav-list"><a href="./index.html" class="nav-link">Home</a></li>
-                <li class="nav-list"><a href="./about.html" class="nav-link">About</a></li>
-                <li class="nav-list"><a href="./service.html" class="nav-link">Services</a></li>
-                <li class="nav-list"><a href="./employee.html" class="nav-link">Employees</a></li>
-                <li class="nav-list"><a href="./product.html" class="nav-link">Products</a></li>
-                <li class="nav-list"><a href="./contact.html" class="nav-link">Contact</a></li>
-                <li class="nav-list"><a href="./login.html" class="nav-link">Login</a></li>
+import React from 'react'
+import { Link } from 'react-router-dom'
 
-            </ul>
+const Navbar = () => {
+  return (
+    <nav className='navbar navbar-dark bg-dark expand-lg'>
+        <Link className="navbar-brand">React Product CRUD</Link>
+
+        <div className='ml auto'>
+        <ul className='navbar nav'>
+            <li className='nav-list'> <link className='nav-link' to="/About">About</link></li>
+            <li className='nav-list'> <link className='nav-link' to="/Product">Product</link></li>
+            <li className='nav-list'> <link className='nav-link' to="/Admin">Admin</link></li>
+            <li className='nav-list'> <link className='nav-link' to="/Contact">Contact</link></li>
+        </ul>
         </div>
-    </nav>  
-    )
-    }
+    </nav>
+  )
 }
 
 export default Navbar
